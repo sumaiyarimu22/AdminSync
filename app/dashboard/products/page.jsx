4f5ec2904323b07c0cfe949341f6a2b9b90160ab
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2023-11-14 22:22:10 +0600

    single user page & add user page added

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,5 +1,67 @@
+import Pagination from "@/app/ui/dashboard/pagination/Pagination";
+import Search from "@/app/ui/dashboard/search/Search";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for a Products..."} />
+        <Link href='/dashboard/products/add'>
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created at</td>
+            <td>stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src='/noproduct.jpg'
+                  alt='noavatar'
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                Iphone
+              </div>
+            </td>
+            <td>pinnacle of innovation in your pocket</td>
+            <td>$999</td>
+            <td>13.01.2022</td>
+            <td>89</td>
+            <td>active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href='/dashboard/products/test'>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default ProductsPage;
