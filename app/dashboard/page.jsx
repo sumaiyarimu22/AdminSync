eac317ab204242c04c3f7b586425c48498019dd5
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2023-11-14 11:56:42 +0600

    card added

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,26 @@
+import Card from "../ui/dashboard/card/Card";
+import Chart from "../ui/dashboard/chart/Chart";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Rightbar from "../ui/dashboard/rightbar/Rightbar";
+import Transaction from "../ui/dashboard/transactions/Transaction";
+
 const DashBoardPage = () => {
-  return <div>DashBoardPage</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transaction />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
 };
 
 export default DashBoardPage;
---
+++ app/ui/dashboard/card/Card.jsx
@@ -0,0 +1,19 @@
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./card.module.css";
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>10.2334</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>10%</span> more than previous week
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
